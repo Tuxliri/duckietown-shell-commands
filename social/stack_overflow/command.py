4544227eff6b96ad7012c60266ed6f458505fe8e
liammcalpineduckietown <liam.mcalpine@duckietown.com>
2025-02-05 11:13:38 +0200

    commands added

---
+++ social/stack_overflow/command.py
@@ -0,0 +1,13 @@
+import webbrowser
+from dt_shell import DTCommandAbs, DTShell
+
+
+class DTCommand(DTCommandAbs):
+    help = "Opens the DT Stack Overflow page"
+
+    @staticmethod
+    def command(shell: DTShell, args, **kwargs):
+        parsed = kwargs.get("parsed", None)
+        if parsed is None:
+            parsed = DTCommand.parser.parse_args(args)
+        webbrowser.open("https://stackoverflowteams.com/c/duckietown/")
