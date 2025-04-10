7f0b3d7177a1c68765458415453ecd113ce5e616
Davide Iafrate <dvde.iafrate98@gmail.com>
2025-04-10 14:03:44 +0200

    Update Jetson Nano 4GB disk image version to 1.4.2 and refactor in_file function

--- init_sd_card/command.py
+++ init_sd_card/command.py
@@ -60,7 +60,7 @@ def DISK_IMAGE_VERSION(robot_configuration, experimental=False):
     board_to_disk_image_version = {
         "raspberry_pi": {"stable": "1.2.1", "experimental": "1.2.1"},
         "raspberry_pi_64": {"stable": "4.0.0", "experimental": "4.0.0"},
-        "jetson_nano_4gb": {"stable": "1.4.1", "experimental": "1.4.1"},
+        "jetson_nano_4gb": {"stable": "1.4.2", "experimental": "1.4.2"},
         "jetson_nano_2gb": {"stable": "1.2.2", "experimental": "1.2.2"},
     }
     board, _ = get_robot_hardware(robot_configuration)
@@ -84,7 +84,7 @@ def PLACEHOLDERS_VERSION(robot_configuration, experimental=False):
         },
         "jetson_nano_4gb": {
             # - stable
-            "1.4.1": "1.1",
+            "1.4.2": "1.1",
             # - experimental
             "-----": "1.1",
         },
@@ -313,8 +313,11 @@ class DTCommand(DTCommandAbs):
                 raise InvalidUserInput(msg)
         # compile hardware specific disk image name and url
         base_disk_image = BASE_DISK_IMAGE(parsed.robot_configuration, parsed.experimental)
+
         # compile files destinations
-        in_file = lambda e: os.path.join(parsed.workdir, f"{base_disk_image}.{e}")
+        def in_file(e):
+            return os.path.join(parsed.workdir, f"{base_disk_image}.{e}")
+
         # notify about licenses
         if "license" not in steps:
             board, _ = get_robot_hardware(parsed.robot_configuration)
